058a4b8d5c800d9614a395f54dcc7b6a9ad88499
Nolulamo <ntshuma959@gmail.com>
2023-12-02 23:45:38 +0200

    My second commit

--- litlelemon/reservation/models.py
+++ litlelemon/reservation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=225)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(99999999999)])
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.name} for {self.booking_date} '
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=225)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory = models.IntegerField(validators=[MaxValueValidator(99999)])
+
+    def __str__(self):
+        return self.title
+
