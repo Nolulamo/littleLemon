f139f78489b5dfaaa216ebebba2fbef8b2c6f904
Nolulamo Tshuma <ntshuma959@gmail.com>
2024-03-04 00:06:30 +0200

    Fix settings file

--- litlelemon/litlelemon/settings.py
+++ litlelemon/litlelemon/settings.py
@@ -87,26 +87,26 @@ CSRF_TRUSTED_ORIGINS = ['https://8001-nolulamo-littlelemon-ysd80eh4d7t.ws-eu108.
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'litlelemon',
-#         'USER': 'root',
-#         'PASSWORD': 'Nolu123456789@23!',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'litlelemon',
+        'USER': 'root',
+        'PASSWORD': 'Nolu123456789@23!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
